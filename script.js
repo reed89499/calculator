cb4c5eb928e23f00cbc425c07c36f06f593ae4d7
Reed89499 <reed89499@gmail.com>
2026-06-02 14:16:38 -0400

    Added operate function
    Added operate function that determines the
    operand and performs relevant mathmatical
    function

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function divide (a, b) {
 let operator = undefined;
 let numA = undefined;
 let numB = undefined;
+
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
