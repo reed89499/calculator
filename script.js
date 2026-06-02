4f015047afd652a7f77989a5aa73931092130476
Reed89499 <reed89499@gmail.com>
2026-06-02 15:24:42 -0400

    Added logic for button press
    On button press, checks what type of button was
    pressed, then will put result into relevant
    variable or evaluate expression

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ function divide (a, b) {
 }
 
 let operator = undefined;
-let numA = undefined;
-let numB = undefined;
+let numA = '';
+let numB = '';
+let operatorSet = false;
 
 function operate(a, b, operator) {
     if (operator === '+') {
@@ -30,3 +31,22 @@ function operate(a, b, operator) {
     }
 }
 
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.classList == 'operator' && button.id != 'equal') { //checks if the button pressed is an operator then sets operator as operator variable, also sets operator set to true
+            operator = button.textContent;
+            operatorSet = true;
+        } else if (button.id == 'equal') { //checks if button is equal, evaluates expression, resets number B and logs result as number A for continued math
+            numA = operate(Number(numA), Number(numB), operator);
+            numB = '';
+            console.log(numA)
+        } else if (button.classList == 'number' && operatorSet == false) { //if operator has not been set yet, puts numbers into numb A
+            numA = numA + button.textContent;
+            console.log(numA);
+        } else if (button.classList == 'number' && operatorSet == true) { //if operator has been set, puts numbers into number B
+            numB = numB + button.textContent;
+            console.log(numB);
+        }
+    })
+});
